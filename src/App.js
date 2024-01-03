3c7394b9c70bf93fa1f80f3b1e867348837d3f77
Joseph Atef <josephatef.j97@gmail.com>
2024-01-03 23:45:26 +0200

    Used Hello Component using JSX

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
-      <Greet/>
-      <Welcome/>
+      {/* <Greet/> */}
+      {/* <Welcome/> */}
+      <Hello></Hello>
     </div>
   );
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello JOZZ</h1>
+        </div>
+    )
+};
+
+export default Hello;

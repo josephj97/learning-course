176c00fa9070d51516e17d5e7464b28d2cfcaf13
Joseph Atef <josephatef.j97@gmail.com>
2024-01-05 22:52:23 +0200

    Used createElement() method

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const Hello = () => {
-    return (
-        <div>
-            <h1>Hello JOZZ</h1>
-        </div>
-    )
+    // return (
+    //     <div>
+    //         <h1>Hello JOZZ</h1>
+    //     </div>
+    // )
+    return React.createElement(
+        'div', 
+        {id: 'div', className: 'dummyClass'}, 
+        React.createElement('h1', null, 'Hello JOZZ!')
+    );
 };
 
 export default Hello;

391cb1db9ccaa88c49464d722059a8d7c0977f0f
Joseph Atef <josephatef.j97@gmail.com>
2024-01-13 03:23:49 +0200

    used setState to increment by 5 and updated state based on the previous state value

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,16 +11,27 @@ class Counter extends Component {
     }
 
     increment(){
-        this.setState({
-            count: this.state.count + 1
-        }, ()=> {console.log(this.state.count)})
+        // this.setState({
+        //     count: this.state.count + 1
+        // }, ()=> {console.log(this.state.count)})
+        this.setState(prevState => ({
+            count: prevState.count + 1
+        }))
+    }
+
+    incrementFive(){
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
     }
 
     render() {
         return (
             <div>
                 <div>Counter - {this.state.count}</div>
-                <button onClick={() => this.increment()}>Increment</button>
+                <button onClick={() => this.incrementFive()}>Increment</button>
             </div>
         )
     }
